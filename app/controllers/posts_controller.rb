4daa46b3c0c4f9779df42de69112acd0fe2498c7
melaleyraud6 <theflare.akg.9pb@gmail.com>
2020-04-11 20:14:26 +0900

    post-function
    画像投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all.order("created_at DESC")  # 投稿一覧 新しい順番に
+    @post = Post.new  # 新規投稿のためのインスタンス
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :image)
   end
 end
