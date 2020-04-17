3112e58182cb58c862b9cc83e6fbb5bf5658ef96
melaleyraud6 <theflare.akg.9pb@gmail.com>
2020-04-17 12:47:42 +0900

    user-registration-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users # ユーザー機能に必要なルーティングを一気に生成してくれるdeviseのメソッド。
   root to: "posts#index"
   resources :posts, only: [:create, :show, :edit, :update, :destroy]
+  resources :users, only: [:show]
 end
